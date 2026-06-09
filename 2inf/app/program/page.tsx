7b0437b4d9212c14d58560969cd1f6b30c14aeab
Vultus Limunaria <danobrovold@gmail.com>
2026-06-09 17:08:27 +0200

    Remove Dark mode and add Foredrag as a Section

--- 2inf/app/program/page.tsx
+++ 2inf/app/program/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/ui/hero";
 import Navbar from "@/components/ui/navbar";
+import PreseentationSection from "@/components/ui/presentationSection";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Typography from "@/components/ui/typography";
 import WorkshopSection from "@/components/ui/workshopsSection";
@@ -13,13 +14,14 @@ export default function Page(){
             </Hero>
 
             <div className="flex justify-center m-5">
-                <Tabs defaultValue="Workshops">
+                <Tabs defaultValue="Workshops" className="flex justify-center items-center">
                     <TabsList variant={"line"}>
                         <TabsTrigger value="Workshops" className="text-xl">Workshops</TabsTrigger>
                         <TabsTrigger value="Foredrag" className="text-xl">Foredrag</TabsTrigger>
                         <TabsTrigger value="Bedrifter" className="text-xl">Bedrifter</TabsTrigger>
                     </TabsList>
                     <TabsContent value="Workshops"><WorkshopSection></WorkshopSection></TabsContent>
+                    <TabsContent value="Foredrag"><PreseentationSection></PreseentationSection> </TabsContent>
                 </Tabs>
             </div>
             
